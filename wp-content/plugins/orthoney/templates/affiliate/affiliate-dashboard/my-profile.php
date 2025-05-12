f4f07833e390a056b0a6aaaa532103bd17a3932f
Designer Pooja <pooja.yourdevexpert@gmail.com>
2025-05-12 15:22:55 +0530

    Design Changes

--- wp-content/plugins/orthoney/templates/affiliate/affiliate-dashboard/my-profile.php
+++ wp-content/plugins/orthoney/templates/affiliate/affiliate-dashboard/my-profile.php
@@ -105,12 +105,17 @@ $tax_id = get_user_meta($user_id, '_yith_wcaf_tax_id', true);
     <div class="affiliate-profile" id="update-price-affiliate-form">
         <form id="affiliate-update-price-form">
             <div class="form-row gfield--width-half">
-                <label for="city">Product Price</label>
-                <input type="text" name="product_price" id="product_price" value="<?php echo esc_attr($product_price); ?>" data-error-message="Please enter a product price">
-                <span class="error-message"></span>
-            </div>
-            <div class="form-row text-right">
-                <button type="button" class="add-user us-btn-style_1" id="affiliate-product-price-profile">Update Price</button>
+                <div class="product-price-box">
+                    <div class="col-left">
+                            <label for="city">Product Price</label>
+                            <input type="text" name="product_price" id="product_price" value="<?php echo esc_attr($product_price); ?>" data-error-message="Please enter a product price">
+                            <span class="error-message"></span>   
+                  </div>    
+        
+                    <div class="col-right text-right">
+                        <button type="button" class="add-user us-btn-style_1" id="affiliate-product-price-profile">Update Price</button>
+                    </div>
+               </div>
             </div>
         </form>
     </div>
