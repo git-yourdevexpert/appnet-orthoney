1c96fc4352992f047539b2729bcb02d721b10314
Designer Pooja <pooja.yourdevexpert@gmail.com>
2025-05-13 16:02:57 +0530

    Design Changes

--- wp-content/plugins/orthoney/classes/class-common-custom.php
+++ wp-content/plugins/orthoney/classes/class-common-custom.php
@@ -525,7 +525,7 @@ class OAM_COMMON_Custom {
         if (is_user_logged_in()) {
             $output .= '<li>Hi, ' . $display_name . '!</li>';
             if (in_array('administrator', $roles)) {
-                $output .= '<li><a href="' . CUSTOMER_DASHBOARD_LINK . '">Customer Area</a></li>';
+                $output .= '<li><a href="' . CUSTOMER_DASHBOARD_LINK . '">Customer Dashboard</a></li>';
                 $output .= '<li><a href="' . ORGANIZATION_DASHBOARD_LINK . '">Organization Area</a></li>';
                 $output .= '<li><a href="' . SALES_REPRESENTATIVE_DASHBOARD_LINK . '">Sales Representative Area</a></li>';
             } else {
