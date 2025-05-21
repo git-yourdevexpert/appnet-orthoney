3437b1521fea5d18831687efa659080f52cae1ef
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-05-21 20:09:09 +0530

    Organizations block and unblock issue

--- wp-content/plugins/orthoney/classes/customer/class-ajax.php
+++ wp-content/plugins/orthoney/classes/customer/class-ajax.php
@@ -2595,6 +2595,12 @@ class OAM_Ajax{
     }
 
     public function orthoney_affiliate_status_toggle_block_handler() {
+
+        /**
+         * 1 : Unblock/Active
+         * 0 : Block/Deactivate
+         * -1 : Pending Request
+         */ 
         global $wpdb;
         $user_id = get_current_user_id();
         $oh_affiliate_customer_linker = OAM_Helper::$oh_affiliate_customer_linker;
@@ -2620,7 +2626,7 @@ class OAM_Ajax{
         );
 
         if ($update_status == 1) {
-            wp_send_json_error(['message' => 'The organization has been blocked.']);
+            wp_send_json_success(['message' => 'The organization has been blocked.']);
         }else{
             wp_send_json_success(['message' => 'The organization has been unblocked.']);
         }
