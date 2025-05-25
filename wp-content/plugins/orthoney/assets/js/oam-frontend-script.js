837bdfad66b43dfe442996a99ef022c42c43978c
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-05-25 12:41:55 +0530

    Order

--- wp-content/plugins/orthoney/assets/js/oam-frontend-script.js
+++ wp-content/plugins/orthoney/assets/js/oam-frontend-script.js
@@ -2661,8 +2661,8 @@ function order_filter_main_order(){
          
             $('#customer-orders-table_filter').append('');
  
-            const tableType = `entries`;
-            $('#customer-orders-table_length').before( tableType );
+            // const tableType = ``;
+            // $('#customer-orders-table_length').before('<div>' + tableType + '</div>');
 
  
             jQuery('#customer-orders-table_filter input[type=search]').off();
