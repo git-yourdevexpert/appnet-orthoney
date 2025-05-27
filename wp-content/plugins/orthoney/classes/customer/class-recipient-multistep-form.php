60d78e9ea17d4295488568659b2885b9078527fd
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-05-27 14:30:48 +0530

    Text Change

--- wp-content/plugins/orthoney/classes/customer/class-recipient-multistep-form.php
+++ wp-content/plugins/orthoney/classes/customer/class-recipient-multistep-form.php
@@ -129,7 +129,7 @@ class OAM_RECIPIENT_MULTISTEP_FORM
         
         <div id="stepNav" class="tab-selections" <?php echo ((self::$atts_process_id != 0) ? 'style="display:none"' : '' ) ?>>
             <span class="step-nav-item <?php echo ($currentStep == 0  AND self::$atts_process_id == 0) ? 'active' : '' ?>" data-step="0">Step 1: Select an Organization</span>
-            <span class="step-nav-item <?php echo $currentStep == 1 ? 'active' : '' ?>" data-step="1">Step 2: Order Method</span>
+            <span class="step-nav-item <?php echo $currentStep == 1 ? 'active' : '' ?>" data-step="1">Step 2: Proceed With Order</span>
             <span class="step-nav-item <?php echo $currentStep == 2 ? 'active' : '' ?>" data-step="2">Step 3: Upload Recipients</span>
             <span class="step-nav-item <?php echo (($currentStep == 3 OR self::$atts_process_id != 0) ? 'active' : '') ?>" data-step="3">Step 4: Add/Review Recipients</span>
             <span class="step-nav-item <?php echo ($currentStep == 4 OR $currentStep == 5) ? 'active' : '' ?>" data-step="4">Step 5: Verify Addresses</span>
