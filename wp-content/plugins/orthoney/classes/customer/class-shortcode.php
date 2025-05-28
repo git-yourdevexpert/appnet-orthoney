cb9645bbc1c11f393b0d2cf06192d0f3de377ca5
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-05-28 15:05:35 +0530

    Update Query

--- wp-content/plugins/orthoney/classes/customer/class-shortcode.php
+++ wp-content/plugins/orthoney/classes/customer/class-shortcode.php
@@ -94,37 +94,38 @@ class OAM_Shortcode
          global $wpdb;
         $user_id = get_current_user_id();
           
-        $query = $wpdb->prepare("
-            SELECT DISTINCT
+       $query = $wpdb->prepare("
+        SELECT DISTINCT
+            rec.affiliate_token AS affiliate_code,
+            orders.id AS order_id,
+            orders.date_created_gmt AS order_date,
+            ba.first_name AS billing_first_name,
+            ba.last_name AS billing_last_name,
+            IFNULL(qty_table.total_quantity, 0) AS total_quantity
+        FROM {$wpdb->prefix}wc_orders AS orders
+        INNER JOIN {$wpdb->prefix}oh_wc_order_relation AS rel 
+            ON rel.wc_order_id = orders.id
+        LEFT JOIN {$wpdb->prefix}oh_recipient_order AS rec 
+            ON rec.order_id = rel.order_id
+        LEFT JOIN {$wpdb->prefix}wc_order_addresses AS ba 
+            ON ba.order_id = orders.id AND ba.address_type = 'billing'
+        LEFT JOIN (
+            SELECT 
                 orders.id AS order_id,
-                orders.date_created_gmt AS order_date,
-                ba.first_name AS billing_first_name,
-                ba.last_name AS billing_last_name,
-                rec.affiliate_token AS affiliate_code,
-                IFNULL(qty_table.total_quantity, 0) AS total_quantity
+                SUM(CAST(oim.meta_value AS UNSIGNED)) AS total_quantity
             FROM {$wpdb->prefix}wc_orders AS orders
-            INNER JOIN {$wpdb->prefix}oh_wc_order_relation AS rel 
-                ON rel.wc_order_id = orders.id
-            LEFT JOIN {$wpdb->prefix}oh_recipient_order AS rec 
-                ON rec.order_id = rel.order_id
-            LEFT JOIN {$wpdb->prefix}wc_order_addresses AS ba 
-                ON ba.order_id = orders.id AND ba.address_type = 'billing'
-            LEFT JOIN (
-                SELECT 
-                    orders.id AS order_id,
-                    SUM(CAST(oim.meta_value AS UNSIGNED)) AS total_quantity
-                FROM {$wpdb->prefix}wc_orders AS orders
-                INNER JOIN {$wpdb->prefix}woocommerce_order_items AS oi 
-                    ON oi.order_id = orders.id AND oi.order_item_type = 'line_item'
-                INNER JOIN {$wpdb->prefix}woocommerce_order_itemmeta AS oim 
-                    ON oim.order_item_id = oi.order_item_id AND oim.meta_key = '_qty'
-                GROUP BY orders.id
-            ) AS qty_table ON qty_table.order_id = orders.id
-            WHERE orders.customer_id = %d
-            AND orders.status NOT IN ('wc-cancelled', 'wc-failed', 'wc-on-hold', 'wc-refunded')
-            ORDER BY orders.date_created_gmt DESC
-            LIMIT %d
-        ", $user_id, $limit);
+            INNER JOIN {$wpdb->prefix}woocommerce_order_items AS oi 
+                ON oi.order_id = orders.id AND oi.order_item_type = 'line_item'
+            INNER JOIN {$wpdb->prefix}woocommerce_order_itemmeta AS oim 
+                ON oim.order_item_id = oi.order_item_id AND oim.meta_key = '_qty'
+            GROUP BY orders.id
+        ) AS qty_table ON qty_table.order_id = orders.id
+        WHERE orders.customer_id = %d
+        AND orders.status NOT IN ('wc-cancelled', 'wc-failed', 'wc-on-hold', 'wc-refunded')
+        ORDER BY orders.date_created_gmt DESC
+        LIMIT %d
+    ", $user_id, $limit);
+
         $row_data = $wpdb->get_results($query);
           
         ?>
