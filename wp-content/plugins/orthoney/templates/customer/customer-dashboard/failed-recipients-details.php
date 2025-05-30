b3831fe307ba94abb2e29fc24b948e93a74255bf
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-05-30 14:54:00 +0530

    nav add

--- wp-content/plugins/orthoney/templates/customer/customer-dashboard/failed-recipients-details.php
+++ wp-content/plugins/orthoney/templates/customer/customer-dashboard/failed-recipients-details.php
@@ -3,6 +3,14 @@ if (!defined('ABSPATH')) {
     exit;
 }
 $recipient_id = get_query_var('failed-recipients-details');
+$dashboard_link = CUSTOMER_DASHBOARD_LINK;
+$dashboard_link_label = 'Return to Dashboard';
+?>
+ <div class="heading-title">
+        <h3 class="block-title">All Incomplete Orders</h3>
+        <a class="w-btn us-btn-style_1" href="<?php echo esc_url( $dashboard_link ) ?>"><?php echo esc_html( $dashboard_link_label ) ?></a>
+    </div>
+<?php 
 
 echo do_shortcode("[recipient_multistep_form]");
 ?>
