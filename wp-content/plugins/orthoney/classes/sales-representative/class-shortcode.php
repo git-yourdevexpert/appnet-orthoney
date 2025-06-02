d37b29d5da7f3c5d3e432e5a8b488ed75cb55281
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-06-02 18:57:42 +0530

    UPdate code

--- wp-content/plugins/orthoney/classes/sales-representative/class-shortcode.php
+++ wp-content/plugins/orthoney/classes/sales-representative/class-shortcode.php
@@ -31,7 +31,7 @@ class OAM_SALES_REPRESENTATIVE_Shortcode
         $user_id = get_current_user_id();
         $user_roles = OAM_COMMON_Custom::get_user_role_by_id($user_id);
        
-        if (in_array('sales_representative', $user_roles) || in_array('administrator', $user_roles)) {
+        if (in_array('sales_representative', $user_roles) || (in_array('administrator', $user_roles) ||  in_array('customer', $user_roles))) {
                 echo OAM_SALES_REPRESENTATIVE_Helper::sales_representative_dashboard_navbar($user_roles);
                  $endpoint = get_query_var('sales_representative_endpoint');
                  $template_path = OH_PLUGIN_DIR_PATH . '/templates/sales-representative/';
