a4462b39d19b54214d52654a50400000c0dd8144
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-06-02 22:04:45 +0530

    Failed Recipient download

--- wp-content/plugins/orthoney/templates/customer/customer-dashboard/groups-details.php
+++ wp-content/plugins/orthoney/templates/customer/customer-dashboard/groups-details.php
@@ -139,7 +139,11 @@ if (!empty($recipients)) :
                         </td>
                         <td data-label="Status">
                             <div class="thead-data">Status</div>
-                            <?php echo ($data->address_verified == 0) ? 'Data Failed' : 'Data Validated'; ?>
+                            <?php 
+                            $decoded_reasons = json_decode($record->reasons, true);
+                            $reasons = (is_array($decoded_reasons)) ? implode(", ", $decoded_reasons) : (string) $decoded_reasons;
+                            echo ($data->address_verified == 0) ? $reasons : 'Data Validated';
+                             ?>
                         </td>
                         <td data-label="Action">
                             <div class="thead-data">Action</div>
