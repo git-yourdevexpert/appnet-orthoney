73c5851a09ed99294b40d881582058b0c792bb50
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-06-09 12:12:51 +0530

    admin new affiliate [mail]

--- wp-content/plugins/orthoney/templates/woocommerce/emails/admin-new-affiliate.php
+++ wp-content/plugins/orthoney/templates/woocommerce/emails/admin-new-affiliate.php
@@ -29,39 +29,49 @@ $user_id = $user->ID;
 if ( ! $user ) {
 	return;
 }
+
+$organization = get_user_meta($user_id, '_yith_wcaf_name_of_your_organization', true)?: '';
+$first_name = get_user_meta($user_id, '_yith_wcaf_first_name', true)?: '';
+$last_name = get_user_meta($user_id, '_yith_wcaf_last_name', true)?: '';
+$city = get_user_meta($user_id, '_yith_wcaf_city', true)?: '';
+$state = get_user_meta($user_id, '_yith_wcaf_state', true)?: '';
+$zipcode = get_user_meta($user_id, '_yith_wcaf_zipcode', true)?: '';
+$tax_id = get_user_meta($user_id, '_yith_wcaf_tax_id', true)?: '';
+$website = get_user_meta($user_id, '_yith_wcaf_your_organizations_website', true)?: '';
+$phone_number = get_user_meta($user_id, '_yith_wcaf_phone_number', true)?: '';
+$oam_heart = get_user_meta($user_id, '_yith_wcaf_oam_heart', true)?: '';
+
 ?>
 <?php do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
 <p>
 	Hello, <?php echo get_bloginfo( 'name' ); ?>
 </p>
 <p>
-	A new organization has successfully registered on the platform and is currently awaiting your approval.
+	A new organization, <?php echo esc_html($organization); ?> from <?php echo esc_html($city) ?>, <?php echo esc_html($state) ?>, has registered on the Honey From The Heart platform and is awaiting your approval.
 </p>
 
 <p>
 	<strong>Organization Details:</strong><br/><br/>
 	
-	<strong>Organization Name: </strong> <span><?php echo get_user_meta($user_id, '_yith_wcaf_name_of_your_organization', true) ?></span>
-	
-	<br/>
-	
-	<strong>
-		<?php echo esc_html_x( 'Email:', '[EMAILS] New affiliate email', 'yith-woocommerce-affiliates' ); ?>
-	</strong>&nbsp;
+	<strong>First Name: </strong> <span><?php echo esc_html($first_name); ?></span><br/>
+	<strong>Last Name: </strong> <span><?php echo esc_html($last_name); ?></span><br/>
+	<strong>Organization Name: </strong> <span><?php echo esc_html($organization); ?></span><br/>
+	<strong>Website: </strong> <span><?php echo esc_html($website); ?></span><br/>
+	<strong>Phone Number: </strong> <span><?php echo esc_html($phone_number); ?></span><br/>
+	<strong>Email: </strong>
 	<span class="email">
 		<a href="mailto:<?php echo esc_attr( $user->user_email ); ?>"><?php echo esc_html( $user->user_email ); ?></a>
 	</span>
-	
 	<br/>
-	<strong>Contact Person: </strong> <span><?php echo get_user_meta($user_id, '_yith_wcaf_first_name', true) ?> <?php echo get_user_meta($user_id, '_yith_wcaf_last_name', true) ?></span><br/>
-	
+	<strong>Tax ID: </strong> <span><?php echo esc_html($tax_id); ?></span><br/>
+	<strong>Full Address: </strong> <span><?php echo esc_html($address); ?></span><br/>
+	<strong>How They Heard About Us: </strong> <span><?php echo esc_html($oam_heart); ?></span><br/>
+
 </p>
 <p>
-	Please review and take the appropriate action through your admin dashboard.
-</p>
-<p>
-	Best regards,  <br/>
-	<?php echo get_bloginfo( 'name' ); ?>
+	Please log into your admin dashboard to review and take the appropriate action.<br>
+	<strong>We're always excited to welcome new partners to our hive.</strong><br>
+	<strong>Let's keep things buzzing!</strong><br>
 </p>
 
 <?php do_action( 'woocommerce_email_footer', $email ); ?>
