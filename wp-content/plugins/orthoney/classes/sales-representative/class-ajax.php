49c80708fc4647c933c9e7facf14e8ac7a440093
Yourdev-Dhaval <buddhadevdhaval.yourdevexpert@gmail.com>
2025-06-19 14:46:30 +0530

    Update code

--- wp-content/plugins/orthoney/classes/sales-representative/class-ajax.php
+++ wp-content/plugins/orthoney/classes/sales-representative/class-ajax.php
@@ -224,16 +224,16 @@ if($user->user_email != ''){
             $search_filter .= $wpdb->prepare(" AND MAX(CASE WHEN um.meta_key = '_yith_wcaf_name_of_your_organization' THEN um.meta_value END) LIKE %s ", $search_like);
         }
 
-        if (!empty($organization_search)) {
-            $search_like = '%' . $wpdb->esc_like($organization_search) . '%';
+        // if (!empty($organization_search)) {
+        //     $search_like = '%' . $wpdb->esc_like($organization_search) . '%';
             
-            $search_filter .= $wpdb->prepare("
-                AND (
-                    MAX(CASE WHEN um.meta_key = '_yith_wcaf_name_of_your_organization' THEN um.meta_value END) LIKE %s
-                    OR af.token LIKE %s
-                )
-            ", $search_like, $search_like);
-        }
+        //     $search_filter .= $wpdb->prepare("
+        //         AND (
+        //             MAX(CASE WHEN um.meta_key = '_yith_wcaf_name_of_your_organization' THEN um.meta_value END) LIKE %s
+        //             OR af.token LIKE %s
+        //         )
+        //     ", $search_like, $search_like);
+        // }
 
 
 
