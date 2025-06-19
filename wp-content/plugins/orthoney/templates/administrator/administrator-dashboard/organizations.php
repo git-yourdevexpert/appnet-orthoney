c753c92ca132bdc3373992ea588bf054e596d295
Yourdev-Dhaval <buddhadevdhaval.yourdevexpert@gmail.com>
2025-06-19 14:56:40 +0530

    Update code

--- wp-content/plugins/orthoney/templates/administrator/administrator-dashboard/organizations.php
+++ wp-content/plugins/orthoney/templates/administrator/administrator-dashboard/organizations.php
@@ -22,7 +22,13 @@ $new_organization = OAM_AFFILIATE_Helper::is_user_created_this_year($user_id) ? 
         max-width: 40px !important;
         word-break: break-word;
     }
-  
+
+    #admin-organizations-results table.dataTable tbody th, table.dataTable tbody td:nth-child(3) {
+        width: 210px !important;
+        max-width:210px !important;
+        word-break: break-word;
+    }
+
 </style>
 <div class="affiliate-dashboard order-process-block">
     <div class="heading-title">
