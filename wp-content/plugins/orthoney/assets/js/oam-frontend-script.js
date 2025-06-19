c32f855d89c257f4d9f0fcab704938a73041fee7
Yourdev-Dhaval <buddhadevdhaval.yourdevexpert@gmail.com>
2025-06-19 14:54:21 +0530

    Update code

--- wp-content/plugins/orthoney/assets/js/oam-frontend-script.js
+++ wp-content/plugins/orthoney/assets/js/oam-frontend-script.js
@@ -2127,6 +2127,7 @@ jQuery(document).ready(function ($) {
     columnDefs: [
     //  { targets: 0, width: "50px" },
       { targets: 1, width: "150px" },
+      { targets: 3, width: "150px" },
        { targets: 5, visible: false,searchable: true },
       { targets: -1, orderable: false, width: "100px" },
         { targets: 0, visible: false,searchable: true },
