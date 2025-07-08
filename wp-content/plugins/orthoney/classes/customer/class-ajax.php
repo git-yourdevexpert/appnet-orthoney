d9b829e40cfd357915fc055ef3dd0724fbbd1ec3
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-07-08 14:32:34 +0530

    Add billing address issue

--- wp-content/plugins/orthoney/classes/customer/class-ajax.php
+++ wp-content/plugins/orthoney/classes/customer/class-ajax.php
@@ -3585,7 +3585,7 @@ class OAM_Ajax{
                
                 $cc = implode(',', array_unique(array_filter([
                     $affiliate_org_email,
-                    $customer_email
+                    // $customer_email
                 ])));
 
                 $subject = '#'. $custom_order_id.' Reordering Form – Honey From The Heart';
