62df370d27e609a3cfa686995ec6aa36c35663a2
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-07-17 18:06:37 +0530

    Update code

--- wp-content/plugins/orthoney/classes/customer/class-helper.php
+++ wp-content/plugins/orthoney/classes/customer/class-helper.php
@@ -424,13 +424,13 @@ class OAM_Helper{
 
             if (!empty($search_terms)) {
                 // We split the conditions instead of duplicating placeholders in one clause
-                // $id_placeholders = implode(',', array_fill(0, count($search_terms), '%s'));
+                $id_placeholders = implode(',', array_fill(0, count($search_terms), '%s'));
                 $code_placeholders = implode(',', array_fill(0, count($search_terms), '%s'));
 
-                $where_conditions[] = "rel.affiliate_code IN ($code_placeholders)";
+                $where_conditions[] = "(CAST(orders.id AS CHAR) IN ($id_placeholders) OR rel.affiliate_code IN ($code_placeholders))";
 
                 // Append values for both placeholders
-                $where_values = array_merge( $search_terms);
+                $where_values = array_merge($search_terms, $search_terms);
             }
            
         }
@@ -524,6 +524,8 @@ class OAM_Helper{
          if (!empty($_REQUEST['search_by_organization'])) {
             if($_REQUEST['search_by_organization'] != 'Orthoney'){
                 $where_conditions[] = "(rel.affiliate_user_id != 0 )";
+            }else{
+                $where_conditions[] = "(rel.affiliate_user_id = 0 )";
             }
         }
 
