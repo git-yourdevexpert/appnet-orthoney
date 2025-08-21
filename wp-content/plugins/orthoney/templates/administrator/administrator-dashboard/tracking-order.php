e77093e3219f0f1d11af3f1d04406835ff3c561d
Yourdev -Ashish <ashishv.yourdevexpert@gmail.com>
2025-08-21 15:03:41 +0530

    Fulfilment code

---
+++ wp-content/plugins/orthoney/templates/administrator/administrator-dashboard/tracking-order.php
@@ -0,0 +1,89 @@
+<?php
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+$current_user_id = get_current_user_id();
+$redirect_back_user_id = get_transient('redirect_back_user_' . $current_user_id);
+
+if ($redirect_back_user_id) {
+    $nonce = wp_create_nonce('auto_login_' . $redirect_back_user_id);
+    $back_url = home_url('/?action=auto_login&user_id=' . $redirect_back_user_id . '&nonce=' . $nonce . '&redirect_to=' . urlencode(SALES_REPRESENTATIVE_DASHBOARD_LINK));
+    echo '<a href="' . esc_url($back_url) . '" class="button">Back to Previous User</a>';
+}
+?>
+<div class="order-block-wrap">
+    <div class="order-process-dashboard">
+        <div class="dashboard-block">
+            <h3 class="block-title">Tracking Order Report</h3>
+            <div class="block-row">
+                <div class="tracking_order_form">
+                    <form id="tracking-order-upload-form" enctype="multipart/form-data">
+                        <div class="file-upload field-block">
+                            <label>
+                            <span class="title-block">
+                                File Upload: <small>(Accept: .csv, .xlsx, OR .xls)</small>
+                            </span>
+                            </label>
+                            <input type="file" accept=".csv, .xlsx, .xls" id="fileInput" name="csv_file" required>
+                        </div>
+                        <br>
+                        <button type="submit" class="w-btn us-btn-style_2">Upload File</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+
+<?php 
+global $wpdb;
+ 
+ $table    = $wpdb->prefix . 'oh_tracking_order';
+$tracking_orders = $wpdb->get_results($wpdb->prepare("SELECT * FROM $table"));
+$upload_dir = WP_CONTENT_URL . '/uploads/fulfillment-reports/tracking-orders/';
+if(!empty($tracking_orders)) {
+?>
+<div class="affiliate-dashboard order-process-block">
+    <div class="heading-title">
+        <h3 class="block-title">Tracking Order File Status</h3>
+        
+    </div>
+    <!-- Search and filter options -->
+    <div class="orthoney-datatable-warraper">
+        <div id="" class="orthoney-datatable-warraper table-with-search-block">
+            <table id="" class="display " style="width:100%">
+                <thead>
+                <tr>
+                    <th>File Name</th>
+                    <th>File Time</th>
+                    <th>Update Type</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($tracking_orders as $order) {
+                    echo '<tr>';
+                    echo '<td>' . esc_html($order->file_name) . '</td>';
+                    echo '<td>' . esc_html($order->file_time) . '</td>';
+                    echo '<td>' . esc_html(ucwords(strtolower($order->uploaded_type))) . '</td>';
+                    echo '<td>' . esc_html(ucwords(strtolower($order->status))) . '</td>';
+                    echo '<td>
+                    <button data-fileid="' . esc_attr($order->id) . '" data-filename="' . esc_attr($order->updated_file_name) . '" class="tracking_order_import_button w-btn us-btn-style_2">Run</button>
+                    <a href="' . esc_url($upload_dir.$order->updated_file_name) . '" class="button-icon-underline"><img src="' . esc_url(OH_PLUGIN_DIR_URL . 'assets/image/download.png') . '" alt="Download">Download File</a>
+                    </td>';
+                    echo '</tr>';
+                }
+                ?>
+            </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php
+} 
+?>
